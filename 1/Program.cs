798b1a72e34e490f1499f5aa216f4e1e16192ec8
Olga <olga-ork@yandex.ru>
2022-12-05 20:33:26 +0300

    Add Task 41

---
+++ 1/Program.cs
@@ -0,0 +1,45 @@
+﻿int [] array = new int[5];
+// void FullArray (int [] Array)
+// {
+//     for (int i = 0; i < Array.Length; i++)
+//     {
+//         Console.WriteLine("Введите числa через запятую: ");
+//         int.TryParse(Console.ReadLine(), out Array[i]);
+//     }
+// }
+
+void PrintArray(int[] array) 
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int PositivNumber(int[] arr)
+{
+    int pos = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0) pos++;
+    }
+    return pos;
+}
+
+void NumberString(string[] array)
+{
+    string[] strings = new string[] {};
+
+    Console.WriteLine("Введите числa через запятую: ");
+    int[] str = Array.ConvertAll(strings, int.Parse);
+    Console.WriteLine(String.Join(",", str));
+} 
+   
+PrintArray(array);
+PositivNumber(array);
+int positivNumber = PositivNumber(array);
+NumberString(array);
+Console.WriteLine($"Положительных чисел -> {positivNumber}");
--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,1 +1,53 @@
-﻿
+﻿// Задача 41. Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+void PrintArray(int[] array) 
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int PositiveNumbers(int[] arr)
+{
+    int pos = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0) pos++;
+    }
+    return pos;
+}
+
+Console.WriteLine("Введите любое количество отрицательных и положительных чисел через запятую: ");
+string numbers = Console.ReadLine();
+
+string[] Separation(string num)
+{
+    char[] spearator = {',', ' '};
+    string[] strlist = num.Split(spearator, StringSplitOptions.RemoveEmptyEntries);
+    return strlist;
+}
+
+int[] ConVertation(string[] strlist)
+{
+int[] convertedarray = new int[strlist.Length];
+for (int i = 0; i < strlist.Length; i++)
+{
+    convertedarray[i] = Convert.ToInt32(strlist[i]);
+}
+return convertedarray;
+}
+
+string[] StrNumbers = Separation(numbers);
+int[] array1 = ConVertation(StrNumbers);
+Separation(numbers);
+Console.WriteLine($"Создаем массив из введенных чисел");
+PrintArray(array1);
+Console.WriteLine($"Положительных чисел -> {PositiveNumbers(array1)}");
